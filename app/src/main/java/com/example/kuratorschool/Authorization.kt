0fcc0cffb0faa63aa5580f1acb6471964155efac
Sonya <petrovacofiya@yandex.ru>
2025-02-17 22:36:10 +0300

    perehod

---
+++ app/src/main/java/com/example/kuratorschool/Authorization.kt
@@ -0,0 +1,28 @@
+package com.example.kuratorschool
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Authorization : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_authorization)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val loginButton: Button = findViewById(R.id.loginButton)
+        loginButton.setOnClickListener{
+            val intent = Intent(this, stud_menu::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
